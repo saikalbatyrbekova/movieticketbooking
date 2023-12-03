bd8ddce63b3e91d9dc2d1a0cf21fe3a52a0a4722
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-03 18:01:44 +0600

    Update Database.java

--- MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/Database.java
+++ MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/Database.java
@@ -1,1 +1,23 @@
+package com.example.movieticketbookingsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String dbURL = "jdbc:postgresql://localhost:5432/movie_booking";
+    private static final String username = "postgres";
+    private static final String password = "1234";
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+            System.out.println("Connected to Database");
+            // You can perform database operations here as needed
+        } catch (SQLException e) {
+            System.out.println("Connection failed. Check the error message:");
+            System.out.println(e.getMessage());
+        }
+    }
+}
 
