513eaab944c724988a5034d67e73057e3ea153f4
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-03 17:56:45 +0600

    Update module-info.java

--- MovieTicketBookingSystem/src/main/java/module-info.java
+++ MovieTicketBookingSystem/src/main/java/module-info.java
@@ -1,6 +1,10 @@
-module com.example.movieticketbookingsystem {
+module com.example.movieticketbookingoop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires fontawesomefx;
+    requires javafx.graphics;
+    requires java.sql;
+
 
 
     opens com.example.movieticketbookingsystem to javafx.fxml;
