bdcfc256caef318716d38677e028e2d6c3a1f069
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-09 14:48:01 +0600

    Update dashboardController.java

--- MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/dashboardController.java
+++ MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/dashboardController.java
@@ -1,24 +1,38 @@
 package com.example.movieticketbookingsystem;
 
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+
 
 public class dashboardController implements Initializable {
 
+    @FXML
+    private AnchorPane topForm;
 
     @FXML
     private Button addMovies_btn;
@@ -27,19 +41,16 @@ public class dashboardController implements Initializable {
     private Button addMovies_clearBtn;
 
     @FXML
-    private TableColumn<?, ?> addMovies_col_date;
+    private TableColumn<moviesData, Date> addMovies_col_date;
 
     @FXML
-    private TableColumn<?, ?> addMovies_col_duration;
+    private TableColumn<moviesData, String> addMovies_col_duration;
 
     @FXML
-    private TableColumn<?, ?> addMovies_col_genre;
+    private TableColumn<moviesData, String> addMovies_col_genre;
 
     @FXML
-    private TableColumn<?, ?> addMovies_col_movieTitle;
-
-    @FXML
-    private TextField addMovies_date;
+    private TableColumn<moviesData, String> addMovies_col_movieTitle;
 
     @FXML
     private Button addMovies_deleteBtn;
@@ -69,7 +80,7 @@ public class dashboardController implements Initializable {
     private TextField addMovies_search;
 
     @FXML
-    private TableView<?> addMovies_tableView;
+    private TableView<moviesData> addMovies_tableView;
 
     @FXML
     private Button addMovies_updateBtn;
@@ -243,6 +254,9 @@ public class dashboardController implements Initializable {
     private Label username;
 
     @FXML
+    private DatePicker addMovies_date;
+
+    @FXML
     void signUp_close(ActionEvent event) {
 
     }
@@ -252,11 +266,378 @@ public class dashboardController implements Initializable {
 
     }
 
+    private Image image;
+    private double x = 0;
+    private double y = 0;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet result;
+    public void importImage(){
+        FileChooser open = new FileChooser();
+        open.setTitle("Open Image File");
+        open.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image File", "*.png", "*.jpg", "*.jpeg"));
+
+        Stage stage = (Stage) addMovies_form.getScene().getWindow();
+        File file = open.showOpenDialog(stage);
+
+        if (file != null) {
+            try {
+                image = new Image(file.toURI().toString(), 141, 177, false, true);
+                addMovies_imageView.setImage(image);
+                getData.path = file.getAbsolutePath();
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+
+    }
+
+    public void movieId() throws SQLException {
+        String sql = "SELECT count(id) FROM movie";
+        connect = database.connectDb();
+
+        try {
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+            if (result.next()) {
+                getData.movieId = result.getInt(1); // Use the index 1 for the first column
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (result != null) result.close();
+                if (prepare != null) prepare.close();
+                if (connect != null) connect.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void insertAddMovies() {
+        String sql1 = "SELECT movie_title FROM movie WHERE movie_title = ?";
+        Alert alert;
+
+        try (Connection connect = Database.connectDb();  // Open the connection in a try-with-resources block
+             PreparedStatement checkStatement = connect.prepareStatement(sql1)) {
+
+            checkStatement.setString(1, addMovies_movieTitle.getText());
+            ResultSet result = checkStatement.executeQuery();
+
+            if (result.next()) {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText(addMovies_movieTitle.getText() + " is already exist!");
+                alert.showAndWait();
+            } else {
+                if (addMovies_movieTitle.getText().isEmpty()
+                        || addMovies_genre.getText().isEmpty()
+                        || addMovies_duration.getText().isEmpty()
+                        || addMovies_imageView.getImage() == null
+                        || addMovies_date.getValue() == null) {
+
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Please fill all blank fields!");
+                    alert.showAndWait();
+                } else {
+                    String sql = "INSERT INTO movie (id, movie_title, genre, duration, image, date) VALUES (?, ?, ?, ?, ?, ?)";
+                    String uri = getData.path.replace("\\", "\\\\");
+
+                    movieId();
+
+                    try (PreparedStatement prepare = connect.prepareStatement(sql)) {
+                        prepare.setInt(1, getData.movieId + 1);
+                        prepare.setString(2, addMovies_movieTitle.getText());
+                        prepare.setString(3, addMovies_genre.getText());
+                        prepare.setString(4, addMovies_duration.getText());
+                        prepare.setString(5, uri);
+                        java.sql.Date sqlDate = java.sql.Date.valueOf(addMovies_date.getValue());
+                        prepare.setDate(6, sqlDate);
+
+                        prepare.execute();
+                        alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Information Message");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Successfully added a new movie!");
+                        alert.showAndWait();
+
+                        showAddMoviesList();
+                        clearAddMovieList();
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateAddMovieList() {
+        String sql = "UPDATE movie SET movie_title = ?, genre = ?, duration = ?, image = ?, date = ? WHERE id = ?";
+        String uri = getData.path.replace("\\", "\\\\");
+
+        try (Connection connect = Database.connectDb();  // Open the connection in a try-with-resources block
+             PreparedStatement statement = connect.prepareStatement(sql)) {
+
+            statement.setString(1, addMovies_movieTitle.getText());
+            statement.setString(2, addMovies_genre.getText());
+            statement.setString(3, addMovies_duration.getText());
+            statement.setString(4, uri);
+            java.sql.Date sqlDate = java.sql.Date.valueOf(addMovies_date.getValue());
+            statement.setDate(5, sqlDate);
+            statement.setInt(6, getData.movieId);
+
+            statement.executeUpdate();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Successfully updated " + addMovies_movieTitle.getText());
+            alert.showAndWait();
+
+            showAddMoviesList();
+            clearAddMovieList();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAddMovies() throws SQLException {
+        String sql = "DELETE FROM movie WHERE movie_title = '"
+                + addMovies_movieTitle.getText() + "' ";
+
+        connect = database.connectDb();
+
+        try{
+            statement = connect.createStatement();
+
+            Alert alert;
+
+
+            if(addMovies_movieTitle.getText().isEmpty()
+                    || addMovies_genre.getText().isEmpty()
+                    || addMovies_duration.getText().isEmpty()
+                    || addMovies_date.getValue() == null
+                    || addMovies_imageView.getImage() == null) {
+
+                alert = new Alert (Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please select the movie first");
+                alert.showAndWait();
+
+            }else{
+
+                alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure to delete " + addMovies_movieTitle.getText()  + "?");
+
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if(ButtonType.OK.equals(option.get())) {
+
+                    statement.executeUpdate(sql);
+                    showAddMoviesList();
+                    clearAddMovieList();
+
+                    alert = new Alert((Alert.AlertType.INFORMATION));
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully deleted!");
+                    alert.showAndWait();
+
+                }else{
+                    return;
+                }
+            }
+
+
+        }catch(Exception e) {e.printStackTrace();}
+    }
+    public void clearAddMovieList(){
+        addMovies_movieTitle.setText("");
+        addMovies_genre.setText("");
+        addMovies_duration.setText("");
+        addMovies_imageView.setImage(null);
+        addMovies_date.setValue(null);
+    }
+
+    private Database database = new Database();
+
+    public ObservableList<moviesData> addMoviesList() {
+        ObservableList<moviesData> listData = FXCollections.observableArrayList();
+        String sql = "SELECT * FROM movie";
+
+        try {
+
+            connect = Database.connectDb();
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            moviesData movD;
+
+            while (result.next()) {
+                movD = new moviesData(result.getInt("id"),
+                        result.getString("movie_title"),
+                        result.getString("genre"),
+                        result.getString("duration"),
+                        result.getString("image"),
+                        result.getDate("date"));
+
+                listData.add(movD);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (result != null) result.close();
+                if (prepare != null) prepare.close();
+                if (connect != null) connect.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return listData;
+    }
+
+
+    public ObservableList<moviesData> listAddMovies;
+
+    public void showAddMoviesList() {
+        listAddMovies = addMoviesList();
+
+        addMovies_col_movieTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        addMovies_col_genre.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        addMovies_col_duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        addMovies_col_date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        addMovies_tableView.setItems(listAddMovies);
+    }
+
+    public void selectAddMoviesList() {
+        moviesData movD = addMovies_tableView.getSelectionModel().getSelectedItem();
+        int num = addMovies_tableView.getSelectionModel().getSelectedIndex();
+
+        if ((num - 1) < -1) {
+            return;
+        }
+
+        getData.path = movD.getImage();
+
+        getData.movieId = movD.getId();
+
+        addMovies_movieTitle.setText(movD.getTitle());
+        addMovies_genre.setText(movD.getGenre());
+        addMovies_duration.setText(movD.getDuration());
+
+
+        String uri = "file:" + movD.getImage();
+
+        image = new Image(uri, 141, 177, false, true);
+        addMovies_imageView.setImage(image);
+
+        LocalDate localDate = Instant.ofEpochMilli(movD.getDate().getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        addMovies_date.setValue(localDate);
+    }
+
+    public void logOut() {
+        signout.getScene().getWindow().hide();
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MovieTicketFXML.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+
+            root.setOnMousePressed((MouseEvent mouseEvent) -> {
+                x = mouseEvent.getSceneX();
+                y = mouseEvent.getSceneY();
+            });
+
+            root.setOnMouseDragged((MouseEvent mouseEvent) -> {
+                stage.setX(mouseEvent.getScreenX() - x);
+                stage.setY(mouseEvent.getScreenY() - y);
+            });
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void switchForm(ActionEvent event) {
+        if (event.getSource() == dashboard_btn) {
+            dashboard_form.setVisible(true);
+            addMovies_form.setVisible(false);
+            availableMovies_form.setVisible(false);
+            editScreening_form.setVisible(false);
+            customers_form.setVisible(false);
+        }
+
+        if (event.getSource() == addMovies_btn) {
+            dashboard_form.setVisible(false);
+            addMovies_form.setVisible(true);
+            availableMovies_form.setVisible(false);
+            editScreening_form.setVisible(false);
+            customers_form.setVisible(false);
+        }
+        if (event.getSource() == availableMovies_btn) {
+            dashboard_form.setVisible(false);
+            addMovies_form.setVisible(false);
+            availableMovies_form.setVisible(true);
+            editScreening_form.setVisible(false);
+            customers_form.setVisible(false);
+        }
+        if (event.getSource() == editScreening_btn) {
+            dashboard_form.setVisible(false);
+            addMovies_form.setVisible(false);
+            availableMovies_form.setVisible(false);
+            editScreening_form.setVisible(true);
+            customers_form.setVisible(false);
+        } else if (event.getSource() == customers_btn) {
+            dashboard_form.setVisible(false);
+            addMovies_form.setVisible(false);
+            availableMovies_form.setVisible(false);
+            editScreening_form.setVisible(false);
+            customers_form.setVisible(true);
+        }
+    }
+
+    public void displayUsername() {
+        username.setText(getData.username);
+    }
+
     public void close() {
         System.exit(0);
     }
+
+    public void minimize() {
+        Stage stage = (Stage) topForm.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        try {
+            displayUsername();
+            showAddMoviesList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
