46a59ec042dbd8c0204093667774c07d25210134
saikalbatyrbekova <132566790+saikalbatyrbekova@users.noreply.github.com>
2023-12-16 23:14:29 +0600

    Update getData.java

--- MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/getData.java
+++ MovieTicketBookingSystem/src/main/java/com/example/movieticketbookingsystem/getData.java
@@ -4,4 +4,11 @@ public class getData {
     public static String username;
     public static String path;
     public static int movieId;
+    public static String title;
+    public static String movieTitle;
+
+    public static int ticket;
+    public static int avMovie;
+    public static double total;
+    public static double totalIncome;
 }
